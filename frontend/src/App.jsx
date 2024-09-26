7b287925c714eae6ec34bc3a555ba5a89c68f134
Satyam19113 <Satyamkr1354@gmail.com>
2024-09-27 00:56:16 +0530

    left side bar panal ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Home from './components/Home'
+
+
+function App() {
+
+  return (
+    <>
+   <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LeftSidebar.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { FaHome } from "react-icons/fa";
+import { FaHashtag } from "react-icons/fa";
+import { IoIosNotifications } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { BsBookmarksFill } from "react-icons/bs";
+import { IoLogOut } from "react-icons/io5";
+import { SiGooglemessages } from "react-icons/si";
+
+
+
+const LeftSidebar = () => {
+  return (
+    <div>
+      <div>
+        <div >
+          <img className='ml-5 ' width={"45px"} src="https://toppng.com/uploads/preview/twitter-logo-png-photo-116617240253ywzn3usl6.png" alt="twitter-logo" />
+        </div>
+        <div className='m-4'>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <FaHome size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Home</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+            <FaHashtag  size="24px"  />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Explore</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <IoIosNotifications size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Notifications</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <SiGooglemessages size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Messages</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <FaUser size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Profile</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <BsBookmarksFill size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Bookmarks</h1>
+          </div>
+          <div className='flex items-center my-2 px-4 py-2 hover:bg-green-200 hover:cursor-pointer  rounded-full'>
+            <div>
+              <IoLogOut size="24px" />
+            </div>
+            <h1 className="font-bold text-lg ml-2" >Logout</h1>
+          </div>
+          <button className='px-4 py-2 border-none text-mg bg-[#1D9BF0] w-full rounded-full text-white font-bold  '>Post</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default LeftSidebar;
